338e3478db5f9d95260fe23dbfdce14a0303ac6c
az275 <az275@cornell.edu>
2024-12-16 19:27:23 +0000

    formatting fix

--- scripts/cpu-gpu-monitor.sh
+++ scripts/cpu-gpu-monitor.sh
@@ -9,7 +9,7 @@ cd data
 while true
 do
     top -n 1 -o +%CPU -b | grep cascade_server | sed -e 's/\s\+/,/g' | ts %s >> cpu_utilization.dat
-    { date +%s | sed -z 's/\n/, /g'; nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv; } >> gpu_utilization.dat
+    { date +%s | sed -z 's/\n/, /g'; nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv,noheader; } >> gpu_utilization.dat
     nvidia-smi --query-compute-apps=process_name,pid,used_memory --format=csv,noheader | ts %s"," >> gpu_by_process.dat
     sleep 3
 done
