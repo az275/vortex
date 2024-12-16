aef15a9155ef399c2bfc8c04002e95de9c49f1d7
az275 <az275@cornell.edu>
2024-12-16 17:40:55 +0000

    better timestamp logging

--- scripts/cpu-gpu-monitor.sh
+++ scripts/cpu-gpu-monitor.sh
@@ -3,10 +3,13 @@
 mkdir data
 cd data
 
+# apt-get install moreutils
+# needed for ts
+
 while true
 do
-    { date +%s | tr -d '\n'; top -n 1 -o +%CPU -b | grep cascade_server | sed -e 's/\s\+/,/g'; } >> cpu_utilization.dat
+    top -n 1 -o +%CPU -b | grep cascade_server | sed -e 's/\s\+/,/g' | ts %s >> cpu_utilization.dat
     { date +%s | sed -z 's/\n/, /g'; nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv; } >> gpu_utilization.dat
-    { date +%s | sed -z 's/\n/, /g'; nvidia-smi --query-compute-apps=process_name,pid,used_memory --format=csv,noheader; } >> gpu_by_process.dat
+    nvidia-smi --query-compute-apps=process_name,pid,used_memory --format=csv,noheader | ts %s"," >> gpu_by_process.dat
     sleep 3
 done
